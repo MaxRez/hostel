aef4d376c95b61dc47fbe9c4cb43c82f56463ecb
MaxRez <maxim-reznik@rambler.ru>
2016-06-21 11:27:44 +0300

    Hostel

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(function() {
+    $("#tabs ").tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    $("#tabs li").removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+});
+
+$(function(){
+    $('.small a').click(function(){
+        var images = $(this).find('img');
+        var imgSrc = images.attr('src');
+
+        $(".big img").attr({ src: imgSrc });
+        $(this).siblings('a').removeClass('active');
+        images.parent().addClass('active');
+        return false;
+    });  });
+
+$(function () {
+    var elWrap = $('#slider'),
+        el =  elWrap.find('img'),
+        indexImg = 1,
+        indexMax = el.length;
+
+    function change () {
+        el.fadeOut(500);
+        el.filter(':nth-child('+indexImg+')').fadeIn(500);
+    }
+
+    function autoCange () {
+        indexImg++;
+        if(indexImg > indexMax) {
+            indexImg = 1;
+        }
+        change ();
+    }
+    var interval = setInterval(autoCange, 3000);
+
+    elWrap.mouseover(function() {
+        clearInterval(interval);
+    });
+    elWrap.mouseout(function() {
+        interval = setInterval(autoCange, 3000);
+    });
+
+    elWrap.append('<span class="next"></span><span class="prev"></span>');
+
+    $('span.next').click(function() {
+        indexImg++;
+        if(indexImg > indexMax) {
+            indexImg = 1;
+        }
+        change ();
+    });
+    $('span.prev').click(function() {
+        indexImg--;
+        if(indexImg < 1) {
+            indexImg = indexMax;
+        }
+        change ();
+    });
+});
